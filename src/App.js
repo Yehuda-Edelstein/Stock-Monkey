0c2fba3376936227f81e98169d50248add94dcfa
Yehuda-Edelstein <yeauthor@gmail.com>
2021-12-03 11:53:17 -0500

    Day 5

--- src/App.js
+++ src/App.js
@@ -1,18 +1,10 @@
 // import React, { useState, useEffect } from 'react';
 import React from 'react';
 import Header from './components/Header';
-import Footer from './components/Footer';
 import Form from './components/Form.jsx';
 import './App.css'
 
 function App(props) {
-
-  // const [stocks, setStocks] = useState([])
-  
-  // const searchOptions = {
-  //   key: process.env.REACT_APP_API_KEY,
-  //   api: `https://finnhub.io/docs/api/v1/`
-  // }
 
   return (
     <div>
