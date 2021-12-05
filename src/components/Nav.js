75f77533623b347354050d7521bbe4ed680b9bc1
Yehuda-Edelstein <yeauthor@gmail.com>
2021-12-05 18:51:18 -0500

    Done

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -4,10 +4,10 @@ function Nav(props) {
     return (
         <div>
             <ul className='nav'>
-                <li><a href='https://finance.yahoo.com/' target='_blank' rel='noreferrer noopener'>Quotes</a></li>
-                <li><a href='https://www.investopedia.com/' target='_blank' rel='noreferrer noopener'>Learn</a></li>
-                <li><a href='https://www.tdameritrade.com/' target='_blank' rel='noreferrer noopener'>Trade</a></li>
-                <li><a href='https://www.coinbase.com/price' target='_blank' rel='noreferrer noopener'>Crypto</a></li>
+                <li><a href='https://finance.yahoo.com/' className='quotes' target='_blank' rel='noreferrer noopener'>Quotes</a></li>
+                <li><a href='https://www.investopedia.com/' className='learn' target='_blank' rel='noreferrer noopener'>Learn</a></li>
+                <li><a href='https://www.tdameritrade.com/' className='trade' target='_blank' rel='noreferrer noopener'>Trade</a></li>
+                <li><a href='https://www.coinbase.com/price' className='crypto' target='_blank' rel='noreferrer noopener'>Crypto</a></li>
             </ul>
         </div>
     );
